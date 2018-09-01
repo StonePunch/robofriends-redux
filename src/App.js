3ddb0a056e49d5db1e95d83c47d768fe44c10694
StonePunch <antonio.lopes1997@gmail.com>
2018-09-01 01:45:32 +0100

    Added the ability to search for robots by name

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, {Component} from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import {robots} from './Robots';
 
-const App = () => {
-    return (
-        <div>
-            <h1>RoboFriends</h1>
-            <CardList robots={robots}/>
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    eSearchChange = (event) => {
+        this.setState({searchfield: event.target.value});
+    }
+
+    render() {
+
+        const filter = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.eSearchChange} />
+                <CardList robots={filter}/>
+            </div>
+        );
+    }
 }
 
 export default App;
